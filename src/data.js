404adb15e1233d47a3946169bbcfe86dc1d19910
Areliz Oyarce <arelizuwc@gmail.com>
2022-03-08 12:57:10 -0500

    showData

--- src/data.js
+++ src/data.js
@@ -1,9 +1,20 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+//export const example = () => {
+  //return 'example';
+//};
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+//export const anotherExample = () => {
+ // return 'OMG';
+//};
+
+
+//Función para mostrar la data
+
+
+export const showData = (objectData) => {
+  for (const property in objectData) {
+    
+    console.log(`${property}: ${objectData[property.name]}`);
+  }  
+}
--- src/main.js
+++ src/main.js
@@ -1,12 +1,28 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import { showData } from './data.js';
+import data from './data/lol/lol.js';
+//import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+showData(data.data)
 
 
-const renderData = () =>{
-    var data ="";
-    console.log(data);
-}
+function seeChampions() {
+    const html = document.getElementById("root");
+    const champions = data.data;
+
+    for (let champion in champions) {
+        let data_champion = champions[champion];
+
+    }
+    return (
+    html.innerHTML = 
+    `<li class="name" id="name${id}">
+<p class=name">${data.name}</p>          
+<p>Key</p>
+<p>Title</p>
+<p>Blurb</p>
+<p>Version</p>
+<p>Imagen</p>
+</li>`
+
+)}
