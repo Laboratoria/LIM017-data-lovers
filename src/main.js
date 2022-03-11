1070c999b0fb82ccc1146826d6208222a6e0659b
7SilviaT <eagles8485@gmail.com>
2022-03-10 22:45:36 -0500

    impresion data en landing page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import {todosLaMovies} from './data.js';
+import {dataMovies} from './data.js';
 import data from './data/ghibli/ghibli.js';
+console.log (data);
 
 let bttnMoreInf = document.getElementById("bttnMoreInf");
 bttnMoreInf.addEventListener("click", function() {
@@ -14,6 +15,23 @@ document.getElementById("tarjetInfo").className = "invisible";
 })
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(todosLaMovies, data);
+console.log(dataMovies, data);
+
+let myArray = data.films;
+/* let datitos = myArray.map(function callback(bar,){
+    return '<li>'+bar.films+' '+bar.films+'</li>'
+  }) */
+/* for (let index=0; index<myArray.length; index++){
+document.getElementById("datitos").innerHTML = myArray[index].title}
+if(index=[0]) */
+
+myArray.forEach((myArray) => {
+    document.getElementById("datitos").innerHTML += '<li>'+ myArray.title+' '+ myArray.description +'</li>';
+  })
+
+
+
+
+
 
 
