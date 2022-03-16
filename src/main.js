d4423756e2c2da92fb0e7dff5c915a7395547ce4
7SilviaT <eagles8485@gmail.com>
2022-03-16 18:27:09 -0500

    Styling landing page-cards display

--- src/main.js
+++ src/main.js
@@ -28,24 +28,22 @@ let myArray = data.films;
 
 myArray.forEach((displayAllData) => {
   document.getElementById("filmsInfo").innerHTML += `
-  <div class="cards">
-  <div class="filmsTitle"> Title: ${displayAllData.title}</div>
-  <br>
-  <div class="filmsDescription"> Description: ${displayAllData.description}</div>
+  <div class="cardContainer">
+   <div class="cardFront">
   <img class="poster" src="${displayAllData.poster}">
+   <h2> Title: ${displayAllData.title}</h2>
+   <p> Year: ${displayAllData.release_date} <span>  ⭐ ${displayAllData.rt_score}</span> </p>
+   </div>
+  <br>
+  <div class="cardBack">
+  <h5> Director:  ${displayAllData.director} </h5>
+  <p1> Description: ${displayAllData.description}</p1>
+   </div>
    <br>
-  </div>
+   </div>
   `
     })
 
-
-
-
-
-
-
-
-  
 
 //'<li>'+index.people+' '+index.people+'</li>'
 /*console.log(data);
